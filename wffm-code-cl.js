6d5348f3c1dc336df70ffc7232aa6ffafa32a010
JSpell <mjspell@gmail.com>
2019-09-06 10:27:54 -0400

    Testing - Additions for CL

---
+++ wffm-code-cl.js
@@ -0,0 +1,108 @@
+(function () {  
+    // Get all the things
+    let thisForm = document.querySelector("form[data-wffm]");
+    let selectControl = thisForm.querySelector("select.form-control");
+    let submitButton = thisForm.querySelector('input[type="submit"]');
+    let formFields = getFormFieldsContainer(thisForm, "legend", "form-fields");
+    let eeNumber = getFormFieldsContainer(thisForm, "label", "company size");
+
+    // The following is only used/available if the .custom-message class is added to WFFM & used on the form field.
+    let customMsg =  thisForm.querySelector('.custom-message input[type=text]'); 
+
+    // Create element for the EE Notice & append to form
+    let eeNotice = document.createElement("span");
+    eeNotice.id = "eeNotice";
+    eeNotice.style.display = "none";
+    eeNotice.innerHTML = customMsg ? customMsg.value : "For the fastest answer to all your questions, reach out to us here, contact your HR representative, or call our Customer Care Center at 866-679-3054. (Hours of operations from 8am-8pm EST Monday – Friday)";
+    thisForm.appendChild(eeNotice);
+
+    // Create element for the Agent Notice & append to form
+    let agNotice = document.createElement("span");
+    agNotice.id = "agNotice";
+    agNotice.style.display = "none";
+    agNotice.innerHTML = customMsg ? customMsg.value : `For the fastest answer to all your questions, start out <a href='https://www.coloniallife.com/about/contact-us/agent-support'>here</a>. Still need help? Our service specialists are here to assist you and can be reached at 800-483-6423. (Hours of operations from 8am-7pm EST Monday – Friday)`;
+    thisForm.appendChild(agNotice);
+
+
+    // This function is used to get the parent/containing element of the elements that we are going to show or hide by matching the text in the inner html.
+    function getFormFieldsContainer(form, element, data) {
+        let items = form.querySelectorAll(element), parent = null;
+
+        let itemArr = [].slice.call(items);  // B/c IE11 doesn't like the spread operator.
+        
+        if(items.length > 0) {
+            itemArr.forEach(function(el) {
+                if(el.innerHTML.toLowerCase().trim() === data) {
+                    parent = el.parentNode;
+                } 
+            });
+        }
+        
+        return parent
+    }
+
+    function showEmployee() {
+        eeNotice.style.display = "";
+        formFields.style.display = "none";
+        submitButton.style.display = "none";
+    }
+
+    function showAgent() {
+        agNotice.style.display = "";
+        formFields.style.display = "none";
+        submitButton.style.display = "none";
+    }
+
+    function showEmployer() {
+        eeNotice.style.display = "none";
+        formFields.style.display = "block";
+        submitButton.style.display = "inline-block";
+        if(eeNumber) {
+            const eeNumInput = eeNumber.querySelector('select');
+            eeNumInput.value = '';
+            eeNumber.style.display = "inline-block";
+        }
+    }
+
+    function showBroker() {
+        eeNotice.style.display = "none";
+        formFields.style.display = "block";
+        submitButton.style.display = "inline-block";
+        if(eeNumber){
+            const eeNumInput = eeNumber.querySelector('select');
+            eeNumber.style.display = "none";
+            eeNumInput.value = '2 - 9';
+        }
+    }
+
+    function defaultState() {
+        eeNotice.style.display = "none";
+        formFields.style.display = "none";
+        submitButton.style.display = "none";
+    }
+
+    selectControl.addEventListener("change", function(e) {
+        switch(selectControl.value) {
+            case "Agent":
+                showAgent();
+                break;
+            case "Employee":
+                showEmployee();
+                break;
+            case "Broker":
+                showBroker();
+                break;
+            case "Employer":
+                showEmployer();
+                break;
+            default:
+                defaultState();
+        }
+    });
+
+    selectControl.addEventListener("keydown", function(e) {
+        if(e.key === "Enter")
+		    e.preventDefault();
+    });
+
+})();
