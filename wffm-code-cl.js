9cef14dd3ce07f50a3412f62cc962155985c69b0
Ivan Town <ivantown@Ivans-MacBook-Pro.local>
2020-01-09 09:57:12 -0500

    thisForm behind if statement

--- wffm-code-cl.js
+++ wffm-code-cl.js
@@ -3,49 +3,52 @@
     const url = window.location.href;
     let isCL = url.indexOf("coloniallife.com") !== -1 ? true : false;
     const thisForm = document.querySelector("form[data-wffm].controlled-form");
-    const selectControl = thisForm.querySelector("select.form-control");
-    const submitButton = thisForm.querySelector('input[type="submit"]');
-    const closeButton = getCloseButton(thisForm);
-    const formFields = getFormFieldsContainer(thisForm, "legend", "form-fields");
-    const eeNumber = getFormFieldsContainer(thisForm, "label", "company size") || getFormFieldsContainer(thisForm, "label", "number of employees");
+    if(thisForm){ 
+        const selectControl = thisForm.querySelector("select.form-control");
+        const submitButton = thisForm.querySelector('input[type="submit"]');
+        const closeButton = getCloseButton(thisForm);
+        const formFields = getFormFieldsContainer(thisForm, "legend", "form-fields");
+        const eeNumber = getFormFieldsContainer(thisForm, "label", "company size") || getFormFieldsContainer(thisForm, "label", "number of employees");
 
-    // Static messages
-    const unumIndvMsg = `For the fastest answer to all your questions, reach out to us <a href='https://www.unum.com/employees/contact-us/submit-question'>here</a>, contact your HR representative, or call our Customer Care Center at 866-679-3054. (Hours of operations from 8am-8pm EST Monday – Friday)`;
-    const clIndvMsg = `For the fastest answer to all your questions, reach out to us <a href='https://www.coloniallife.com/individuals/policyholder-support'>here</a>, contact your HR representative, or call our Customer Care Center at 800-325-4368. (Hours of operations from 8am-8pm EST Monday – Friday)`;
-    const clAgntMsg = `For the fastest answer to all your questions, start out <a href='https://www.coloniallife.com/about/contact-us/agent-support'>here</a>. Still need help? Our service specialists are here to assist you and can be reached at 800-483-6423. (Hours of operations from 8am-7pm EST Monday – Friday)`;
-    
-    // The following is only used/available if the .custom-message class is added to WFFM & used on the form field.
-    const customMsg =  thisForm.querySelector('.custom-message input[type=text]'); 
+        // Static messages
+        const unumIndvMsg = `For the fastest answer to all your questions, reach out to us <a href='https://www.unum.com/employees/contact-us/submit-question'>here</a>, contact your HR representative, or call our Customer Care Center at 866-679-3054. (Hours of operations from 8am-8pm EST Monday – Friday)`;
+        const clIndvMsg = `For the fastest answer to all your questions, reach out to us <a href='https://www.coloniallife.com/individuals/policyholder-support'>here</a>, contact your HR representative, or call our Customer Care Center at 800-325-4368. (Hours of operations from 8am-8pm EST Monday – Friday)`;
+        const clAgntMsg = `For the fastest answer to all your questions, start out <a href='https://www.coloniallife.com/about/contact-us/agent-support'>here</a>. Still need help? Our service specialists are here to assist you and can be reached at 800-483-6423. (Hours of operations from 8am-7pm EST Monday – Friday)`;
+        
+        // The following is only used/available if the .custom-message class is added to WFFM & used on the form field.
+        const customMsg =  thisForm.querySelector('.custom-message input[type=text]'); 
 
-    // Create element for the EE Notice & append to form
-    const eeNotice = document.createElement("span");
-    eeNotice.id = "eeNotice";
-    eeNotice.style.display = "none";
-    eeNotice.innerHTML = customMsg ? customMsg.value : isCL ? clIndvMsg : unumIndvMsg; // Yes, these are 2 ternary operators strung together. Yes, IK it's not the best practice. It'll be ok tho.
-    thisForm.appendChild(eeNotice);
+        // Create element for the EE Notice & append to form
+        const eeNotice = document.createElement("span");
+        eeNotice.id = "eeNotice";
+        eeNotice.style.display = "none";
+        eeNotice.innerHTML = customMsg ? customMsg.value : isCL ? clIndvMsg : unumIndvMsg; // Yes, these are 2 ternary operators strung together. Yes, IK it's not the best practice. It'll be ok tho.
+        thisForm.appendChild(eeNotice);
 
-    // Create element for the Agent Notice & append to form
-    const agNotice = document.createElement("span");
-    agNotice.id = "agNotice";
-    agNotice.style.display = "none";
-    agNotice.innerHTML = customMsg ? customMsg.value : clAgntMsg;
-    thisForm.appendChild(agNotice);
+        // Create element for the Agent Notice & append to form
+        const agNotice = document.createElement("span");
+        agNotice.id = "agNotice";
+        agNotice.style.display = "none";
+        agNotice.innerHTML = customMsg ? customMsg.value : clAgntMsg;
+        thisForm.appendChild(agNotice);
 
-    // The following may be needed later to handle multiple of the same forms on a page.  It's probably best to have seperate forms.
+        // The following may be needed later to handle multiple of the same forms on a page.  It's probably best to have seperate forms.
 
-    // function handleForms() {
-    //     let forms = document.querySelectorAll("form[data-wffm].controlled-form");
-    //     let formArray = [].slice.call(forms);
+        // function handleForms() {
+        //     let forms = document.querySelectorAll("form[data-wffm].controlled-form");
+        //     let formArray = [].slice.call(forms);
 
-    //     formArray.forEach(function(form){
-    //         let currentForm = form;
-    //         const selectControl = thisForm.querySelector("select.form-control");
-    //         const submitButton = thisForm.querySelector('input[type="submit"]');
-    //         const formFields = getFormFieldsContainer(thisForm, "legend", "form-fields");
-    //         const eeNumber = getFormFieldsContainer(thisForm, "label", "company size") || getFormFieldsContainer(thisForm, "label", "number of employees");
-    //     });
+        //     formArray.forEach(function(form){
+        //         let currentForm = form;
+        //         const selectControl = thisForm.querySelector("select.form-control");
+        //         const submitButton = thisForm.querySelector('input[type="submit"]');
+        //         const formFields = getFormFieldsContainer(thisForm, "legend", "form-fields");
+        //         const eeNumber = getFormFieldsContainer(thisForm, "label", "company size") || getFormFieldsContainer(thisForm, "label", "number of employees");
+        //     });
 
-    // }
+        // }
+
+    } // end if(thisForm)
 
 
     // This function is used to get the parent/containing element of the elements that we are going to show or hide by matching the text in the inner html.
